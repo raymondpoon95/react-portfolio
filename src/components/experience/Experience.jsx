a874127d8e688bf98f9d5bfdbf5bb94f76fd7454
Raymond Poon <raymondpoon95@hotmail.co.uk>
2021-07-25 22:23:15 +0100

    added timeLine for experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,11 @@
 import "../../styles/experience.scss";
+import TimeLine from "../timeline/TimeLine";
 
 const Experience = () => {
   return (
     <div className="experience" id="experience">
-      experience
+      <div className="title"><h1>Experience</h1></div>
+      <TimeLine />
     </div>
   );
 }
