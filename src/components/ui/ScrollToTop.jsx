4caa0c6903b5b94b949afd17a63f83bd764da56e
Raymond Poon <raymondpoon95@hotmail.co.uk>
2021-08-09 22:19:31 +0100

    run build

--- src/components/ui/ScrollToTop.jsx
+++ src/components/ui/ScrollToTop.jsx
@@ -1,28 +1,25 @@
 // TO DO 
 // SCROLL TO TOP BUTTON 
-
 import React, { useEffect, useState } from "react";
-import arrowIcon from "../../assets/icons/up-arrow.svg";
+import { animateScroll as scroll } from 'react-scroll';
+import { IoIosArrowUp } from "react-icons/io";
 import "../../styles/scrollToTop.scss";
-import { ReactComponent as ArrowIcon} from "../../assets/icons/up-arrow.svg";
+
 
 const ScrollToTop = () => {
-  const [isVisible, setIsVisible] = useState(false);
+  // const [isVisible, setIsVisible] = useState(false);
 
-  const toggleVisibility = () => {
-    if (window.pageYOffset > 100) {
-      console.log(window.pageyOffset);
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
+  // const toggleVisibility = () => {
+  //   if (window.pageYOffset > 100) {
+  //     console.log(window.pageyOffset);
+  //     setIsVisible(true);
+  //   } else {
+  //     setIsVisible(false);
+  //   }
+  // };
 
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
+  function scrollToTop() {
+    scroll.scrollToTop();
   };
 
   // useEffect(() => {
@@ -34,11 +31,16 @@ const ScrollToTop = () => {
 
   return (
     <div className="up-arrow">
-      {isVisible && 
-        <div onClick={scrollToTop}>
-          <img src={arrowIcon} alt="up-arrow" onClick={scrollToTop}></img>
+      {/* {isVisible && 
+        <div>
+          <img src={arrowIcon} alt="up-arrow" onClick={() => scrollToTop()}></img>
         </div>
-      }
+      } */}
+      <div onClick={() => scrollToTop()}>
+        <IoIosArrowUp 
+          className="arrow"
+        />
+      </div>
     </div>
   )
 }
